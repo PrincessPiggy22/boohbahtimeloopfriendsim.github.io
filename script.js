e0d4245d2223d5f133ae007ab52f6e42caa4789a
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2026-03-10 11:52:25 +0000

    edited script

--- script.js
+++ script.js
@@ -4,15 +4,3 @@ document.getElementById("start").onclick = function() {
   window.location.href = "intro.html";
 
 };
-
-
-
-let StoryText = document.getElementById("text"); // getting the text
-
-const story = [
-    {
-        name: "",
-        text: "",
-        image: ""
-    },
-];
