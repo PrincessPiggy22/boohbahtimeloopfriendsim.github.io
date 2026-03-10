6eeb68ff8c43141ec59887767f721d5f54c68887
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2026-03-10 16:04:03 +0000

    teststst

--- rooms/DomeCar.js
+++ rooms/DomeCar.js
@@ -33,33 +33,10 @@ You = "../sprites/you.png";
 // Begning convo
 const character1 =[You,Bling,You,Bling,You,Bling];
 const dialouge1 = ["&quot;What happened?&quot;","&quot;Zing Zing Zingbah's dead&quot;","&quot;Who are you?&quot;","&quot;Blingbah, I run the casino&quot;","&quot;Gambling?&quot;","&quot;Not for You&quot;"];
+
+const character2 =[You];
+const dialouge2 = ["Skibbbb"];
+
 console.log("There is an imposter among us");
 goThruDialouge("roomText", dialouge1, character1);
-
-
-// Click on Zing dead
-const zingDeadChars = [ZingDead, ZingDead, ZingDead, ZingDead, ZingDead, "../sprites/GreenFur.png", "../sprites/GreenFur.png",""];
-const zingDeadTalk = ["Damn... he dead...","...","rip","...","What's that??","Green... fur???","I should talk to Blingbah", ""];
-const zingDeadDidTalk =["I don't think i'll find anything else", ""];
-inventory.push("Green Fur");
-
-
-document.getElementById("ZingDead").addEventListener("click", function() {
-if(inventory.includes("Green Fur")) {
-  console.log("No, Dead bodies don't talk");
-  goThruDialouge("roomText", zingDeadDidTalk, "");
-} else {
-  console.log("Do Dead bodies talk?");
-  goThruDialouge("roomText", zingDeadTalk, zingDeadChars);
-}
-
-});
-
-document.getElementById("Bling").addEventListener("click", function() {
-  if(inventory.includes("Green Fur")) {
-    console.log("You have the greeeeennnnnn");
-  } else {
-    console.log("You don't have the greeeeennnnnn");
-    goThruDialouge("roomText", dialouge1, character1);
-  }
-});  
+goThruDialouge("roomText", dialouge2, character2);
