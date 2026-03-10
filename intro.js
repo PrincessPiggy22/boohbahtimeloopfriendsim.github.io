340cca9a0b8f1201c80d6d01ed99a1f3c455c8a5
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2026-03-10 12:31:14 +0000

    made a js for the intro

---
+++ intro.js
@@ -0,0 +1,21 @@
+document.getElementById("wakeUpButton").onclick = function() {
+
+  window.location.href = "PlayerRoom.html";
+
+};
+
+// the dream
+const dreamTxt = ["You won a lottery allowing you to board the fabled Boohbah express before it opens to the public", "You're first day here was exciting and tired you out", "But you won't remain asleep  for long.."]
+
+let dreamIndex = 0;
+
+document.addEventListener("click", function() {
+
+  dreamIndex++;
+
+  if(dreamIndex < dreamTxt.length) {
+
+    document.getElementById("dreamText").textContent = dreamTxt[dreamIndex];
+    
+  }
+});
--- script.js
+++ script.js
@@ -5,10 +5,14 @@ document.getElementById("start").onclick = function() {
 
 };
 
-document.getElementById("wakeUpButton").onclick = function() {
 
-  window.location.href = "PlayerRoom.html";
 
-};
 
-const dreamTxt = ["You won a lottery allowing you to board the fabled Boohbah express before it opens to the public", "You're first day here was exciting and tired you out", "But you won't remain asleep  for long.."]
+
+
+
+
+
+
+
+
