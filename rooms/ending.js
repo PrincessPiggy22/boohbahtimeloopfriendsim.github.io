45fff00108a16936c8cf9ee829feea7477b59641
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2026-03-12 23:40:08 +0000

    finally done with the game, call me phil collins bc i don't care anymore

---
+++ rooms/ending.js
@@ -0,0 +1,42 @@
+
+
+// SET UP
+// function to be able to click thru dialouge
+function goThruDialouge(id, array) {
+
+  let index = 0;
+  document.getElementById(id).textContent = array[0];
+
+  document.addEventListener("click", function() {
+  index++;
+
+  if(index < array.length) {
+
+    document.getElementById(id).textContent = array[index];
+    
+  }
+});
+};
+
+const inventory = [];
+
+// Start Button
+document.getElementById("start").onclick = function() {
+  console.log("start game");
+  window.location.href = "rooms/intro.html";
+
+};
+
+
+
+// if key not in inventory --> don't open door 
+
+
+
+
+
+
+
+
+
+
